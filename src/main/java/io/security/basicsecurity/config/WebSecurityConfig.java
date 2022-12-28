a76dcfc4d6438679f735d451174b0907f93ce1b6
jhha <hjh7887@gmail.com>
2022-12-29 06:43:41 +0900

    add spring security remember-me

--- src/main/java/io/security/basicsecurity/config/WebSecurityConfig.java
+++ src/main/java/io/security/basicsecurity/config/WebSecurityConfig.java
@@ -4,12 +4,14 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
@@ -21,6 +23,9 @@ import java.io.IOException;
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig {
+
+    @Autowired
+    UserDetailsService userDetailsService;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -67,9 +72,13 @@ public class WebSecurityConfig {
                                 response.sendRedirect("/login");
                             }
                         })
-                        .deleteCookies("remember-me")
+                        .deleteCookies("remember")
                         .permitAll()
-                );
+                )
+                .rememberMe()
+                .rememberMeParameter("remember")
+                .tokenValiditySeconds(3600)
+                .userDetailsService(userDetailsService);
 
         return http.build();
     }
